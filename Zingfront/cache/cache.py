d9ccf89fce667576f394a83fd131cadabdfba3e0
10171618 <xu.yong25@zte.com.cn>
2017-10-31 20:01:35 +0800

    zingfront

---
+++ Zingfront/cache/cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import time
+import random
+
+# object is not need in python3
+class Cache:
+    def __init__(self, id, age):
+        self.id = id
+        self.age = age
+        self.next = None
+
+
+    def insert(self):
+
+
+    def update(self):
+
+
+
+
+if __name__ == "__main__":
+    print("hello")
